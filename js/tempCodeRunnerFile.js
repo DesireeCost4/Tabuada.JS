91a49c5a1ad62275d2c575c467301b3368a1ba96
Desireê <deehco1996@gmail.com>
2023-11-17 18:28:56 -0300

    Primeiro commit - upando arquivo

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+onst createTable = (number, multiplicatorNumber) => {
+
+    multiplicationTable.innerHTML="";
+
+ for( i = 1; i <= multiplicatorNumber; i++) {
+
+    const result = number * i;
+
+    console.log(result)
